b2c6d76bc79fb82eb6c509ef69b185174678abc1
Grzegorz <kalarus.g@gmail.com>
2018-10-10 18:16:21 +0200

    validation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
     const $nameInput = $('.nameInput');
     const $pswInput = $('.pswInput');
+    const $nameGroup = $('.nameGroup');
+    const $pswGroup = $('.pswGroup');
+    const $loginPar = $('<p>Login musi składać się z minimum 3 znaków</p>');
+    const $pswPar = $('<p>Hasło musi składać się z minimum 3 znaków</p>');
     
     $nameInput.on('keypress', function() {
         console.log($nameInput.val())
@@ -15,10 +19,15 @@ $( document ).ready(function() {
             if($nameInput.val().length < 3 && $pswInput.val().length < 3) {
                 $nameInput.attr('placeholder', 'Proszę podać nazwę użytkownika');
                 $pswInput.attr('placeholder', 'Proszę podać hasło');
+                $nameGroup.append($loginPar);
+                $pswGroup.append($pswPar);
             }else if($nameInput.val().length >= 3 && $pswInput.val().length < 3) {
                 $pswInput.attr('placeholder', 'Proszę podać hasło');
-            }else {
+                $nameGroup.find('p').remove();
+
+            }else if($pswInput.val().length >= 3 && $nameInput.val().length < 3) {
                 $nameInput.attr('placeholder', 'Proszę podać nazwę użytkownika');
+                $pswGroup.find('p').remove();
             }
         }
     })
