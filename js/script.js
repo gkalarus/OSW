0269e5a84ff2a49c5678f45754eac86ef1b16caf
Grzegorz <kalarus.g@gmail.com>
2018-10-10 09:38:27 +0200

    simple validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 $( document ).ready(function() {
+    const $nameInput = $('.nameInput');
+    const $pswInput = $('.pswInput');
+    
+    $nameInput.on('keypress', function() {
+        console.log($nameInput.val())
+    })
+
     const $logBtn = $('.log');
     $logBtn.on('click', function(e) {
         e.preventDefault();
-        window.location.href= "welcome.html";
+        if($nameInput.val().length >= 3 && $pswInput.val().length >= 3) {
+            window.location.href= "welcome.html";
+        }else {
+            if($nameInput.val().length < 3 && $pswInput.val().length < 3) {
+                $nameInput.attr('placeholder', 'Proszę podać nazwę użytkownika');
+                $pswInput.attr('placeholder', 'Proszę podać hasło');
+            }else if($nameInput.val().length >= 3 && $pswInput.val().length < 3) {
+                $pswInput.attr('placeholder', 'Proszę podać hasło');
+            }else {
+                $nameInput.attr('placeholder', 'Proszę podać nazwę użytkownika');
+            }
+        }
     })
 });
